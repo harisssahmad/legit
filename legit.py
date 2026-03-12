62b5a45067e3f2ee7ba7b006d99c78147868d71a
Haris Ahmad <me.haris2003@gmail.com>
2026-03-13 00:04:12 +0500

    Add commit functionality

--- legit.py
+++ legit.py
@@ -1,3 +1,23 @@
+class Commit():
+    def __init__(self, id, message):
+        self.id = id
+        self.message = message
+
 class Legit():
     def __init__(self, name):
-        self.name = name
+        self.name = name # repo name
+        self.last_commit_id = 0
+
+    def commit(self, message):
+        new_commit = Commit(self.last_commit_id, message)
+        self.last_commit_id += 1
+        return new_commit
+
+
+repo = Legit("my_repo")
+# Actual command:
+# > git init
+
+repo.commit("Initial commit")
+# Actual command:
+# > git commit -m "Make commit work"
